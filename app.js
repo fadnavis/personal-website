06cbf96f9721c2a5dcb49ec27e2af06d108279a6
fadnavis <harsh.fad@gmail.com>
2017-05-10 17:59:31 +0200

    add about route to app.js

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get("/",function(req,res){
   res.render("landing");
 });
 
+app.get("/about",function(req,res){
+  res.render("about");
+});
 
 app.listen(8080,process.env.IP,function(){
   console.log("app has started listening at port");
