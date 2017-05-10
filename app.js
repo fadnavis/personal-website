a5893426e18179eeb25dd993a685d5912b87bdd6
fadnavis <harsh.fad@gmail.com>
2017-05-10 17:00:45 +0200

    add env.PORT

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/",function(req,res){
 });
 
 
-app.listen(8080,process.env.IP,function(){
+app.listen(process.env.PORT,process.env.IP,function(){
   console.log("app has started listening at port");
 });
