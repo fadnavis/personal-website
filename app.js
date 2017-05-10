1ecd264f60c74051851274dcd561b1248eee68cc
fadnavis <harsh.fad@gmail.com>
2017-05-10 16:52:43 +0200

    restructure files

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require("express");
 
 var app = express();
+app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 app.get("/",function(req,res){
-  res.render("index.html");
+  res.render("index");
 });
 
 
-app.listen(process.env.PORT,process.env.IP,function(){
-  console.log("app has started listening");
+app.listen(8080,process.env.IP,function(){
+  console.log("app has started listening at port");
 });
